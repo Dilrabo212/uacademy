6052db4efac1ddc622a23c1646ba1a612b22f29c
dilrabo <dilraboxujamuratova@gmail.com>
2023-08-08 13:08:49 +0500

    update

---
+++ REACT/ToDoList/src/component/Todo/todo.js
@@ -0,0 +1,104 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import './todo.css'
+
+// export const Todo = () => {
+
+
+
+//     const url = "https://64d0fc61ff953154bb79cfb7.mockapi.io/todo"
+
+//     fetch(url, {
+//         method: "POST",
+//         body: JSON.stringify({
+//             nametitle: input.value,
+//         }),
+//         headers: {
+//             "Content-type": "application/json"
+//         }
+//     })
+//         .then(response => response.json())
+//         .then(data => {
+//         })
+
+
+//     fetch(url)
+//         .then((res) => res.json())
+//         .then((data) => {
+//             console.log(data)
+
+//             data.map((item) => {
+
+//             })
+//         });
+// }
+
+
+// return (
+//     <>
+//         <div className="container">
+//             <h1>To Do list APP</h1>
+
+//             <div className="body">
+//                 <form class="forma">
+
+//                     <input class="input" type="text" placeholder="input text" required />
+//                     <button class="submit">Add</button>
+//                 </form>
+
+//                 <ul class="list">
+
+//                 </ul>
+//                 <button class="btn">Clear all</button>
+//             </div>
+//         </div>
+
+
+
+
+
+//     </>
+// );
+
+// export default Todo
+
+
+
+
+
+
+
+
+
+
